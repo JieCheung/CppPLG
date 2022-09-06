07fadb5258a3295cc5b1cff7204e787f195382cf
zhangjie <6578989+zhangjie9705@user.noreply.gitee.com>
2022-09-06 23:21:58 +0800

    "first"

---
+++ hjj/hjj/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hjj
+//
+//  Created by 张杰 on 2022/9/6.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
